b66f4b406d8c21d9e083d7cc6e8c0b8369fe4567
Wojciech Szlosek <wojciech185sz@gmail.com>
2021-04-15 15:53:18 +0200

    sorting lists, datatime operations

--- Source/main.py
+++ Source/main.py
@@ -1,5 +1,7 @@
 # Wojciech Szlosek
 
+from datetime import datetime
+
 def getDate(listOfLines):
 
     date = []
@@ -7,9 +9,9 @@ def getDate(listOfLines):
     for l in listOfLines:
         date.append(l[0].strip())
 
-    print(date)
 
     return date
+
 
 def getStatuses(listOfStatuses):
 
@@ -18,9 +20,9 @@ def getStatuses(listOfStatuses):
     for l in listOfStatuses:
         statuses.append(l[1].strip())
 
-    print(statuses)
 
     return statuses
+
 
 def getDoorCodes(listOfCodes):
 
@@ -29,9 +31,19 @@ def getDoorCodes(listOfCodes):
     for l in listOfCodes:
         codes.append(l[2].strip())
 
-    print(codes)
-
     return codes
+
+
+def getFrontDoorsCode(date, statuses, codes): # ???
+
+    frontDoors = []
+
+    for i in range(1, len(date)):
+        if (date[i][0:10] != date[i-1][0:10] and "exit" in statuses[i-1]):
+            frontDoors.append(codes[i-1])
+
+    return frontDoors
+
 
 def openAndSeparate(filename):
 
@@ -40,28 +52,65 @@ def openAndSeparate(filename):
 
     # first line does not contain data of interest:
     flag = True
+    len = 0
 
     for line in f:
+
+        len += 1
+
+        if line == "\n":
+            continue
+
         if flag:
+            line = line.replace(" ", "")
+            c = line[4]
             flag = False
             continue
 
-        fields.append(line.split(";"))
+        if line.count(c) != 2:
+            print(f"Błędne dane wejściowe w linii numer {len}! "
+                  f"Rezultat działania programu jej nie uwzględni.")
+            continue
 
-    print(fields)
+        fields.append(line.split(c))
+
+
     date = getDate(fields)
     statuses = getStatuses(fields)
     codes = getDoorCodes(fields)
+
+    drzwiWejsciowe = getFrontDoorsCode(date, statuses, codes)
+    print(drzwiWejsciowe)
 
     f.close()
 
     return(date, statuses, codes)
 
+
+def sortLists(date, statuses, codes):
+
+    sorted_lists = sorted(zip(date, statuses, codes),
+                          key=lambda x: datetime.strptime(x[0], "%Y-%m-%d %H:%M:%S"))
+    date, statuses, codes = [[x[i] for x in sorted_lists] for i in range(3)]
+
+    return(date, statuses, codes)
+
+
+def differentTime(t1, t2):
+
+    w1 = datetime.strptime(t1, "%Y-%m-%d %H:%M:%S")
+    w2 = datetime.strptime(t2, "%Y-%m-%d %H:%M:%S")
+
+    return w1-w2
+
+
 def program(filename):
 
     date, statuses, codes = openAndSeparate(filename)
+    date, statuses, codes = sortLists(date, statuses, codes)
+    print(differentTime(date[21], date[14]))
+
 
 if __name__ == '__main__':
-    program("input.csv")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    program("input.csv")
